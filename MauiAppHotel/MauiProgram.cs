d86fca4dee8acaf4d3912f9e80f0b3a4a76f9a06
Giulia Grossi <giuliaggfatec@gmail.com>
2025-05-24 13:13:42 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppHotel/MauiProgram.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace MauiAppHotel
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("SourGummy_Expanded-Bold.ttf", "SourGummyBold");
+                    fonts.AddFont("SourGummy_Expanded-BoldItalic.ttf", "SourGummyBoldItalic");
+                    fonts.AddFont("SourGummy_Expanded-Italic.ttf", "SourGummyItalic");
+                    fonts.AddFont("SourGummy_Expanded-Regular.ttf", "SourGummy");
+                });
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
